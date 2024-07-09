05a7f188bb759d9f51f0192e26d04a6aae0e8b04
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-07-09 10:56:31 -0700

    add additional method for sort by birthdate

--- index.js
+++ index.js
@@ -44,7 +44,8 @@ console.log(fullNames);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
-const sortedByBirthdate = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
+const sortedByBirthdate = inventors.sort((a, b) => a.year - b.year);
+// const sortedByBirthdate = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
 // const sortedByBirthdate = inventors.sort(function(a, b) {
 //     if (a.year > b.year) {
 //         return 1;
